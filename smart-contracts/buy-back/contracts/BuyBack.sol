4e801e6a36ee209cd6b388db508a9b28a605b40c
Schalk Dormehl <schalk.dormehl@gmail.com>
2020-02-27 12:22:33 +0200

    added comment to buyback.sol

--- smart-contracts/buy-back/contracts/BuyBack.sol
+++ smart-contracts/buy-back/contracts/BuyBack.sol
@@ -3,9 +3,9 @@ pragma solidity ^0.5.0;
 // intent:
 // * to build contract that perpetually uses it's balance of DAI to buy and burn some other token
 // * a design goal is to make sure that everyone can get a fair price for their tokens
-// * we 
+// * we don't want a situation where some people exit early at the costs of others, ie accidentally ponzify the system
 
 contract BuyBack
 {
-
+    
 }
