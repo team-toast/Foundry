d1000b3470ebdba3a552346072359549a5289306
Schalk Dormehl <schalk.dormehl@gmail.com>
2020-05-06 14:26:43 +0200

    Gutted FRY token Moved initialization logic into Deployer.sol

---
+++ smart-contracts/bucket-sale/contracts/Deployer.sol
@@ -0,0 +1,53 @@
+pragma solidity ^0.5.0;
+
+import {FRY as FRY} from "../../fry-token/contracts/FRY.sol";
+import "./BucketSale.sol";
+import "./Forwarder.sol";
+
+
+contract Deployer
+{
+    using SafeMath for uint256;
+
+    constructor(
+            address _teamToastMultisig,
+            uint _startOfSale,
+            uint _bucketPeriod,
+            uint _bucketSupply,
+            uint _bucketCount,
+            IERC20 _tokenSoldFor
+            )
+        public
+    {
+        // Create the treasury contract, giving initial ownership to the Team Toast multisig
+        Forwarder governanceTreasury = new Forwarder(_teamToastMultisig);
+
+        // Create the FRY token
+        FRY fryToken = new FRY();
+
+        // Create the bucket sale
+        BucketSale bucketSale = new BucketSale (
+            address(governanceTreasury),
+            _startOfSale,
+            _bucketPeriod,
+            _bucketSupply,
+            _bucketCount,
+            ERC20Mintable(address(fryToken)),
+            _tokenSoldFor);
+
+        // 30% disbursed to Team Toast
+        fryToken.mint(_teamToastMultisig, uint(30000000).mul(10 ** uint256(fryToken.decimals())));
+
+        // 10% given to the governance treasury
+        fryToken.mint(address(governanceTreasury), uint(10000000).mul(10 ** uint256(fryToken.decimals())));
+
+        // Team Toast will have minting rights via a multisig, to be renounced as various Foundry contracts prove stable and self-organizing
+        fryToken.addMinter(_teamToastMultisig);
+
+        // Give the bucket sale minting rights
+        fryToken.addMinter(address(bucketSale));
+
+        // Have this contract renounce minting rights
+        fryToken.renounceMinter();
+    }
+}
--- smart-contracts/fry-token/contracts/FRY.sol
+++ smart-contracts/fry-token/contracts/FRY.sol
@@ -9,28 +9,8 @@ contract FRY is Context, ERC20Detailed, ERC20Mintable, ERC20Burnable
 {
     using SafeMath for uint;
 
-    constructor(
-            address _teamToastTokenReceiver,
-            address _foundationMultisig,
-            address _forPublicTokenReceiver)
+    constructor()
         public
         ERC20Detailed("Foundry Logistics Token", "FRY", 18)
-    {
-        // Out of 100 million tokens,
-
-        // 30% disbursed to Team Toast
-        _mint(_teamToastTokenReceiver, uint(30000000).mul(10 ** uint256(decimals())));
-
-        // 10% given to the Foundry Foundation
-        _mint(_foundationMultisig, uint(10000000).mul(10 ** uint256(decimals())));
-
-        // 60% set aside to be disbursed via one or more public token events
-        _mint(_forPublicTokenReceiver, uint(60000000).mul(10 ** uint256(decimals())));
-
-        // MinterRole constructor makes msg.sender a minter. Remove this role.
-        _removeMinter(_msgSender());
-
-        // The Foundation will control a minting address via a multisig, to be renounced as various Foundry contracts prove stable and self-organizing
-        _addMinter(_foundationMultisig);
-    }
+    { }
 }
