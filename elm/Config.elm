f0ab597da96324b14e53a0fa2ae487a320a5ea66
coinop-logan <coinop.logan@gmail.com>
2020-10-01 13:19:07 +0000

    remove specific values from Config for master branch

--- elm/Config.elm
+++ elm/Config.elm
@@ -11,12 +11,12 @@ import TokenValue exposing (TokenValue)
 
 mainnetHttpProviderUrl : String
 mainnetHttpProviderUrl =
-    "https://07a14c9f5130471d81dbe1488f0c22f5.eth.rpc.rivet.cloud/"
+    Debug.todo ""
 
 
 kovanHttpProviderUrl : String
 kovanHttpProviderUrl =
-    "https://kovan.infura.io/v3/e3eef0e2435349bf9164e6f465bd7cf9"
+    Debug.todo ""
 
 
 ganacheProviderUrl : String
@@ -28,61 +28,61 @@ daiContractAddress : TestMode -> Address
 daiContractAddress testMode =
     case testMode of
         None ->
-            Eth.Utils.unsafeToAddress "0x6B175474E89094C44Da98b954EedeAC495271d0F"
+            Debug.todo ""
 
         TestKovan ->
             Debug.todo ""
 
         TestMainnet ->
-            Eth.Utils.unsafeToAddress "0x6B175474E89094C44Da98b954EedeAC495271d0F"
+            Debug.todo ""
 
         TestGanache ->
-            Eth.Utils.unsafeToAddress "0x2612Af3A521c2df9EAF28422Ca335b04AdF3ac66"
+            Debug.todo ""
 
 
 fryAddress : TestMode -> Address
 fryAddress testMode =
     case testMode of
         None ->
-            Eth.Utils.unsafeToAddress "0x6c972b70c533E2E045F333Ee28b9fFb8D717bE69"
+            Debug.todo ""
 
         TestKovan ->
             Debug.todo ""
 
         TestMainnet ->
-            Eth.Utils.unsafeToAddress "0xe8c7495870f63DD045ba20E4604Ef3534ffa3724"
+            Debug.todo ""
 
         TestGanache ->
-            Eth.Utils.unsafeToAddress "0x67B5656d60a809915323Bf2C40A8bEF15A152e3e"
+            Debug.todo ""
 
 
 bucketSaleAddress : TestMode -> Address
 bucketSaleAddress testMode =
     case testMode of
         None ->
-            Eth.Utils.unsafeToAddress "0x30076fF7436aE82207b9c03AbdF7CB056310A95A"
+            Debug.todo ""
 
         TestKovan ->
             Debug.todo ""
 
         TestMainnet ->
-            Eth.Utils.unsafeToAddress "0xEB997be36d9a3168e548f058FF6E76Ba16bd8d13"
+            Debug.todo ""
 
         TestGanache ->
-            Eth.Utils.unsafeToAddress "0x26b4AFb60d6C903165150C6F0AA14F8016bE4aec"
+            Debug.todo ""
 
 
 bucketSaleScriptsAddress : TestMode -> Address
 bucketSaleScriptsAddress testMode =
     case testMode of
         None ->
-            Eth.Utils.unsafeToAddress "0xf0B2B0A7bcd35cc119E12Cf4F04B2739Ac400bef"
+            Debug.todo ""
 
         TestKovan ->
             Debug.todo ""
 
         TestMainnet ->
-            Eth.Utils.unsafeToAddress "0xf0B2B0A7bcd35cc119E12Cf4F04B2739Ac400bef"
+            Debug.todo ""
 
         TestGanache ->
             Debug.todo ""
@@ -90,22 +90,22 @@ bucketSaleScriptsAddress testMode =
 
 gasstationApiEndpoint : String
 gasstationApiEndpoint =
-    "https://ethgasstation.info/api/ethgasAPI.json?api-key=ebca374685809a499c4513455cb6867c6112269da20bda9ae64d491a02cf"
+    Debug.todo ""
 
 
 bucketSaleBucketInterval : TestMode -> Time.Posix
 bucketSaleBucketInterval testMode =
-    Time.millisToPosix <| 1000 * 60 * 60 * 7
+    Debug.todo ""
 
 
 bucketSaleTokensPerBucket : TestMode -> TokenValue
 bucketSaleTokensPerBucket testMode =
-    TokenValue.fromIntTokenValue 30000
+    Debug.todo ""
 
 
 bucketSaleNumBuckets : Int
 bucketSaleNumBuckets =
-    2000
+    Debug.todo ""
  
 
 feedbackEndpointUrl : String
