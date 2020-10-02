2a0004cb98eb0a533471fd6493876342c14dcb48
coinop-logan <coinop.logan@gmail.com>
2020-10-02 15:01:14 +0000

    Fix script

--- updatepermafrost.sh
+++ updatepermafrost.sh
@@ -6,7 +6,7 @@ branch_name="$(git symbolic-ref HEAD 2>/dev/null)" ||
 branch_name="(unnamed branch / detached head)"
 branch_name=${branch_name##refs/heads/}
 
-if [ $branch_name != "permafrost_live" ]; then
+if [ $branch_name != "permafrost-live" ]; then
   echo "Not on the right branch! Exiting."
   exit 1
 fi
@@ -14,11 +14,11 @@ fi
 echo "compressing dist/"
 tar -cf dist.tar dist
 
-echo "Updating sale.foundrydao.com"
+echo "Updating permafrost.foundrydao.com"
 echo "transferring dist.tar"
-scp -i $2 dist.tar $1@sale.foundrydao.com:~/update/
+scp -i $2 dist.tar $1@permafrost.foundrydao.com:~/update/
 echo "calling remote update script"
-ssh -i $2 $1@sale.foundrydao.com '\/bin/update.sh'
+ssh -i $2 $1@permafrost.foundrydao.com '\/bin/update.sh'
 
 echo "remote update finished, removing tar"
 rm dist.tar
