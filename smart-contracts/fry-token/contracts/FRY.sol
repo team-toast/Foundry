b01dcedb35ae34384332f5c55b32d019037fb3d4
sjalq <schalk.dormehl@gmail.com>
2020-12-14 12:05:42 +0200

    LETH experiment

--- smart-contracts/fry-token/contracts/FRY.sol
+++ smart-contracts/fry-token/contracts/FRY.sol
@@ -5,12 +5,60 @@ import "../../common.5/openzeppelin/token/ERC20/ERC20Mintable.sol";
 import "../../common.5/openzeppelin/token/ERC20/ERC20Burnable.sol";
 import "../../common.5/openzeppelin/GSN/Context.sol";
 
-contract FRY is Context, ERC20Detailed, ERC20Mintable, ERC20Burnable
+contract LETH is Context, ERC20Detailed, ERC20Mintable, ERC20Burnable
 {
     using SafeMath for uint;
 
-    constructor()
+    constructor(
+            address payable _gulper,
+            address _vault)
         public
-        ERC20Detailed("Foundry Logistics Token", "FRY", 18)
+        ERC20Detailed("Leveraged Ether", "LETH", 18)
     { }
+
+    uint constant FEE_PERC public = 900;
+    uint constant ONE_PERC public = 1000;
+    uint constant HUNDRED_PERC public = 100000;
+
+    function issue(address _receiver)
+        payable
+        public
+    { 
+        // Goals:
+        // 1. deposits it into the vault 
+        // 2. gives the holder a claim on the vault for later withdrawal
+
+        // Logic:
+        // *check how much ether there is in the vault
+        // *check how much debt the vault has
+        // *calculate how much the vault is worth in Ether if it were closed now.
+        // *deposit msg.balance into the vault - fee
+        // *send fee to the gulper contract
+        // *give the minter a  proportion of the LETH such that it represents their value add to the vault
+
+        uint ethValue = vault.collateral().sub(vault.debt().div(vault.price()));
+        uint proportion = msg.value.mul(HUNDRED_PERC).div(ethValue);
+        uint LETHToIssue = totalSupply().mul(proportion).div(HUNDRED_PERC);
+        uint fee = msg.value.div(HUNDRED_PERC).mul(FEE_PERC);
+        vault.deposit()((msg.value.sub(fee));
+        gulper.deposit()(fee);
+        mint(_receiver, LETHToMind);
+    }
+
+    function claim(uint _amount)
+        public
+    {
+        // Goals:
+        // 1. if the _amount being claimed does not drain the vault to below 160%
+        // 2. pull out the amount of ether the senders' tokens entitle them to and send it to them
+
+        uint ethValue = vault.collateral().sub(vault.debt().div(vault.price()));
+        uint proportion = _amount.mul(HUNDRED_PERC).div(this.totalSupply());
+        uint ETHToClaim = ethValue.mul(proportion).div(HUNDRED_PERC);
+        uint fee = ETHToClaim.div(HUNDRED_PERC).mul(FEE_PERC);
+        vault.withdraw(ETHToClaim);
+        burn(msg.sender, _amount);
+        msg.sender.send(ETHToClaim.sub(fee));
+        gulper.deposit()(fee);
+    }
 }
